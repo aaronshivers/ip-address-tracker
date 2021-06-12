dc8ffecf4a6e8785a70092bfda437ea4744a3ba5
aaronshivers <42848750+aaronshivers@users.noreply.github.com>
2021-06-12 08:44:16 -0500

    add fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import Form from './components/Form';
+import Address from './components/Address';
+
+const data = [
+  {
+    key: 'IP Address',
+    value: '192.212.174.101',
+  }, {
+    key: 'location',
+    value: 'Brooklyn, NY 10001',
+  }, {
+    key: 'timezone',
+    value: 'utc -05:00',
+  }, {
+    key: 'isp',
+    value: 'SpaceX Starlink',
+  },
+];
 
 const App = () => (
   <>
-    <Form/>
+    <Form />
+    <Address data={data} />
   </>
 );
 
--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import AddressItem from './AddressItem';
 
-const Address = props => {
-  return (
-    <div>
-
-    </div>
-  );
-};
+const Address = ({ data }) => (
+  <ul>
+    {
+      data.map(({ key, value }) => (
+        <AddressItem key={key} label={key} value={value} />
+      ))
+    }
+  </ul>
+);
 
 Address.propTypes = {
-
+  data: PropTypes.arrayOf(
+    PropTypes.exact({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
 };
 
 export default Address;
