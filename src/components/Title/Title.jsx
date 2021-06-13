90345177d0765d28200b1215f75ddfd3ad0b0bee
aaronshivers <42848750+aaronshivers@users.noreply.github.com>
2021-06-13 11:08:03 -0500

    add config files

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './Title.scss';
 
 const Title = () => (
-  <div className="flex">
-    <h1>
-      IP Address Tracker
-    </h1>
-  </div>
+  // <div className="flex">
+  <h1>
+    IP Address Tracker
+  </h1>
+  // </div>
 );
 
 export default Title;
